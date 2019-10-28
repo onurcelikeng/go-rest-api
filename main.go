d96de6ae99bd6cc591c0674ca01c8d7e820a8695
Onur Celik <onurantmil@gmail.com>
2019-10-28 22:41:11 +0300

    setup http server via gorilla mux

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome GO page!")
+}
 
 func main() {
-	fmt.Println("Hello, GO!")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
